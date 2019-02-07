ede48d720f8aa76c6170a39f0abb47920142fb0c
Justin Wickenheiser <wickenju@gvsu.edu>
2019-02-07 09:09:35 -0500

    Added TDD tests and validations

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,4 +2,7 @@ class Bug < ApplicationRecord
 	enum issue_type: [:Issue, :Enhancement, :Feature]
 	enum priority: [:Low, :Medium, :High]
 	enum status: [:Open, :Closed, :Monitor]
+
+	validates :title, presence: true
+	validates :description, presence: true
 end
--- test/models/bug_test.rb
+++ test/models/bug_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class BugTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@bug = Bug.create(title: "This is my Title", description: "blah blah blah", issue_type: 0, priority: 1, status: 2)
+	end
+
+	test "bug must be valid" do
+		assert @bug.valid?
+	end
+
+	test "Title cannot be blank" do
+		@bug.title = ""
+		assert_not @bug.valid?
+	end
+
+	test "Description cannot be blank" do
+		@bug.description = ""
+		assert_not @bug.valid?
+	end
+
+	test "issue_type must be valid" do
+		invalid_types = [-25, 0, 2, 4, 50]
+		invalid_types.each do |i|
+			begin
+				@bug.issue_type i
+				assert false, "#{i} should be invalid"
+			rescue
+				assert true
+			end
+		end
+	end
+
+	test "priority must be valid" do
+		invalid_priorities = [-25, 0, 2, 4, 50]
+		invalid_priorities.each do |i|
+			begin
+				@bug.issue_type i
+				assert false, "#{i} should be invalid"
+			rescue
+				assert true
+			end
+		end
+	end
+
+	test "status must be valid" do
+		invalid_statuses = [-25, 0, 2, 4, 50]
+		invalid_statuses.each do |i|
+			begin
+				@bug.issue_type i
+				assert false, "#{i} should be invalid"
+			rescue
+				assert true
+			end
+		end
+	end
 end
